c1544f3d02a83a13530a70ec7e0d4ab93808aeae
José Dyego Henrique Cardoso de Sousa <116648398+dyegohrq@users.noreply.github.com>
2025-02-07 12:23:57 -0300

    .

---
+++ assets/js/script.js
@@ -0,0 +1,159 @@
+const body = document.querySelector('body')
+
+
+let product = [
+    {
+       'image': {
+            "thumbnail": "./assets/images/image-waffle-thumbnail.jpg",
+            "mobile": "./assets/images/image-waffle-mobile.jpg",
+            "tablet": "./assets/images/image-waffle-tablet.jpg",
+            "desktop": "./assets/images/image-waffle-desktop.jpg"
+       },
+       "name": "Waffle with Berries",
+       "category": "Waffle",
+       "price": 6.50
+    },
+    {
+        "image": {
+            "thumbnail": "./assets/images/image-creme-brulee-thumbnail.jpg",
+            "mobile": "./assets/images/image-creme-brulee-mobile.jpg",
+            "tablet": "./assets/images/image-creme-brulee-tablet.jpg",
+            "desktop": "./assets/images/image-creme-brulee-desktop.jpg"
+        },
+        "name": "Vanilla Bean Crème Brûlée",
+        "category": "Crème Brûlée",
+        "price": 7.00
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-macaron-thumbnail.jpg",
+            "mobile": "./assets/images/image-macaron-mobile.jpg",
+            "tablet": "./assets/images/image-macaron-tablet.jpg",
+            "desktop": "./assets/images/image-macaron-desktop.jpg"
+        },
+        "name": "Macaron Mix of Five",
+        "category": "Macaron",
+        "price": 8.00
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-tiramisu-thumbnail.jpg",
+            "mobile": "./assets/images/image-tiramisu-mobile.jpg",
+            "tablet": "./assets/images/image-tiramisu-tablet.jpg",
+            "desktop": "./assets/images/image-tiramisu-desktop.jpg"
+        },
+        "name": "Classic Tiramisu",
+        "category": "Tiramisu",
+        "price": 5.50
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-baklava-thumbnail.jpg",
+            "mobile": "./assets/images/image-baklava-mobile.jpg",
+            "tablet": "./assets/images/image-baklava-tablet.jpg",
+            "desktop": "./assets/images/image-baklava-desktop.jpg"
+        },
+        "name": "Pistachio Baklava",
+        "category": "Baklava",
+        "price": 4.00
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-meringue-thumbnail.jpg",
+            "mobile": "./assets/images/image-meringue-mobile.jpg",
+            "tablet": "./assets/images/image-meringue-tablet.jpg",
+            "desktop": "./assets/images/image-meringue-desktop.jpg"
+        },
+        "name": "Lemon Meringue Pie",
+        "category": "Pie",
+        "price": 5.00
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-cake-thumbnail.jpg",
+            "mobile": "./assets/images/image-cake-mobile.jpg",
+            "tablet": "./assets/images/image-cake-tablet.jpg",
+            "desktop": "./assets/images/image-cake-desktop.jpg"
+        },
+        "name": "Red Velvet Cake",
+        "category": "Cake",
+        "price": 4.50
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-brownie-thumbnail.jpg",
+            "mobile": "./assets/images/image-brownie-mobile.jpg",
+            "tablet": "./assets/images/image-brownie-tablet.jpg",
+            "desktop": "./assets/images/image-brownie-desktop.jpg"
+        },
+        "name": "Salted Caramel Brownie",
+        "category": "Brownie",
+        "price": 4.50
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-panna-cotta-thumbnail.jpg",
+            "mobile": "./assets/images/image-panna-cotta-mobile.jpg",
+            "tablet": "./assets/images/image-panna-cotta-tablet.jpg",
+            "desktop": "./assets/images/image-panna-cotta-desktop.jpg"
+        },
+        "name": "Vanilla Panna Cotta",
+        "category": "Panna Cotta",
+        "price": 6.50
+     }
+]
+
+
+function addCart() {
+    container()
+}
+
+function container() {
+    const container = document.createElement('div')
+    container.classList = 'container'
+
+    const h1 = document.createElement('h1')
+    h1.innerText = 'Desserts'
+    container.appendChild(h1)
+
+    product.map((item) => {
+        const section = document.createElement('section')
+        section.classList = 'product_cart'
+
+        const product = document.createElement('div')
+        product.classList = 'product'
+
+        const imgCart = document.createElement('div')
+        imgCart.classList = 'img_cart'
+        imgCart.style.backgroundImage = `url="${item.image.mobile}"`
+                
+        product.appendChild(imgCart)
+
+        const button = document.createElement('button')
+        product.appendChild(button)
+        button.innerHTML = '<i><img src="assets/images/icon-add-to-cart.svg" alt=""></i> Add to cart'
+
+        section.appendChild(product)
+        container.appendChild(section)
+
+        console.log(item)
+    })
+    
+    body.prepend(container)
+}
+
+function verificarTamanhoTela(e) {
+    if (e.matches) {
+        console.log("A tela agora tem 768px ou menos!");
+    } else {
+        console.log("A tela agora tem mais de 768px!");
+    }
+}
+
+let mediaQuery = window.matchMedia("(max-width: 768px)");
+mediaQuery.addEventListener("change", verificarTamanhoTela); // Observa mudanças
+verificarTamanhoTela(mediaQuery); // Verifica no carregamento
+
+
+
+addCart()
