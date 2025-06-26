2353d1e770f35b351e2003df669e1de1d4ceccee
José Dyego Henrique Cardoso de Sousa <116648398+dyegohrq@users.noreply.github.com>
2025-06-26 11:36:56 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,59 @@
 import data from "../data.json";
 import style from "./components/typography.module.css";
-import illustration from '/assets/images/illustration-empty-cart.svg'
+import illustration from "/assets/images/illustration-empty-cart.svg";
 import { Button, type ItemProps } from "./components/button";
 import { useState } from "react";
 
 export default function App() {
-  const [product, setProduct] = useState<ItemProps[]>([])
-  const updateData = data.map((item: any )=> ({
-    ...item, 
+  const [product, setProduct] = useState<ItemProps[]>([]);
+  const updateData = data.map((item) => ({
+    ...item,
     amount: 0,
-    total: 0
-  }))
+    total: 0,
+  }));
 
-    function handleCLick(item: ItemProps ) {
-        setProduct((productItem) => [...productItem, item])
+  function handleAddToCart(item: ItemProps) {
+    const exists = product.some((p) => p.name === item.name)
+
+    if (!exists) {
+      setProduct( (prev) => [...prev, {...item, amount: 1, total: item.price}] )
     }
 
-    console.log(product)
+  }
+
+  function decrementAmount( name:string ) {
+    setProduct( 
+      (prev) => prev.map((item) => 
+        item.name === name ? 
+        {
+          ...item,
+          amount: item.amount - 1,
+          total: (item.amount - 1) * item.price
+        } :
+          item
+      ) .filter( (item) => item.amount > 0 )
+    )
+  }
+
+  function incrementAmount( name:string ) {
+    setProduct(
+      (prev) => prev.map((item) => 
+        item.name === name ?
+        {
+          ...item,
+          amount: item.amount + 1,
+          total: (item.amount + 1) * item.price
+        } :
+        item
+      ) 
+    )
+  }
+
+  function getAmount(name:string) {
+    const found = product.find((p) => p.name === name)
+
+    return found ? found.amount : 0
+  }
 
   return (
     <div className=" p-6 ">
@@ -25,55 +62,101 @@ export default function App() {
         Desserts{" "}
       </h1>
 
-      <div className="lg:flex justify-around " >
-        <div className=" grid grid-cols-1 sm:grid-cols-3 gap-6 lg:max-w-[70%] " >
-          {updateData.map((item) => (
-            <div className=" pt-8 pb-6 " key={item.name} >
-              <div className="relative flex flex-col items-center ">
-                <div className=" rounded-lg overflow-hidden ">
-                  <img
-                    className=" sm:hidden w-full h-full "
-                    src={item.image.mobile}
-                    alt={item.name}
-                  />
-                  <img
-                    className=" hidden sm:block lg:hidden w-full h-full "
-                    src={item.image.tablet}
-                    alt={data[0].name}
-                  />
-                  <img
-                    className=" hidden lg:block w-full h-full "
-                    src={item.image.mobile}
-                    alt={item.name}
-                  />
+      <div className="lg:flex justify-around ">
+        <div className=" grid grid-cols-1 sm:grid-cols-3 gap-6 lg:max-w-[70%] ">
+          {updateData.map((item) => {
+            const isAdded = product.some( (p) => p.name === item.name );
+            const amount = getAmount(item.name)
+
+            return (
+              <div className=" pt-8 pb-6 " key={item.name}>
+                <div className="relative flex flex-col items-center ">
+                  <div className=" rounded-lg overflow-hidden ">
+                    <img
+                      className=" sm:hidden w-full h-full "
+                      src={item.image.mobile}
+                      alt={item.name}
+                    />
+                    <img
+                      className=" hidden sm:block lg:hidden w-full h-full "
+                      src={item.image.tablet}
+                      alt={data[0].name}
+                    />
+                    <img
+                      className=" hidden lg:block w-full h-full "
+                      src={item.image.mobile}
+                      alt={item.name}
+                    />
+                  </div>
+                  <Button 
+                    item={item} 
+                    onClick={handleAddToCart} 
+                    isAdded={isAdded} 
+                    onDecrease={() => decrementAmount(item.name) }
+                    onIncrease={() => incrementAmount(item.name) }
+                    amount={amount}
+                    />
                 </div>
-                <Button item={item} onClick={handleCLick} />
+                <div className=" mt-9 ">
+                  <span
+                    className={`${style["text-present-4"]} text-rose-500  `}
+                  >
+                    {" "}
+                    {item.category}{" "}
+                  </span>
+                  <h2 className={`${style["text-present-3"]} text-rose-900 `}>
+                    {" "}
+                    {item.name}{" "}
+                  </h2>
+                  <span className={`${style["text-present-3"]} text-red `}>
+                    {" "}
+                    {item.price.toLocaleString("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    })}{" "}
+                  </span>
+                </div>
               </div>
-              <div className=" mt-9 ">
-                <span className={`${style["text-present-4"]} text-rose-500  `}>
-                  {" "}
-                  {item.category}{" "}
-                </span>
-                <h2 className={`${style["text-present-3"]} text-rose-900 `}>
-                  {" "}
-                  {item.name}{" "}
-                </h2>
-                <span className={`${style["text-present-3"]} text-red `}>
-                  {" "}
-                  {item.price.toLocaleString("en-US", {
-                    style: "currency",
-                    currency: "USD",
-                  })}{" "}
-                </span>
-              </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
-        <div className=" my-8 p-6 lg:max-w-[30%] " >
-          <h1 className={`${style['text-present-2']} text-red `} >Your Cart (0)</h1>
-          <div className=" p-6 flex flex-col items-center gap-4 " >
-            <img src={illustration} alt="illustration-empty-cart" />
-            <span className={`${style['text-present-4-bold']} text-rose-500  `} >Your added items will appear here</span>
+        <div className=" my-8 p-6 lg:max-w-[30%] ">
+          <h1 className={`${style["text-present-2"]} text-red `}>
+            Your Cart ( { product.reduce( (acc, item) => acc + item.amount, 0 ) } )
+          </h1>
+          <div className="">
+            {
+              product.length === 0 ? (
+                <div>
+                  <img src={illustration} alt="illustration-empty-cart" />
+                  <span className={`${style["text-present-4-bold"]} text-rose-500  `}>
+                    Your added items will appear here
+                  </span>
+                </div>
+              ) : product.map((item) => (
+                <ul>
+                  <li>
+                    <div>
+                      <p> {item.name} </p>
+                      <div>
+                        <span> {item.amount}x </span>
+                        <span> @ {item.price.toLocaleString( 'en-US', {
+                          style: 'currency',
+                          currency: 'USD'
+                        } ) } </span>
+                        <span> { item.total.toLocaleString( 'en-US', {
+                          style: 'currency',
+                          currency: 'USD'
+                        } ) } </span>
+                      </div>
+                    </div>
+                    <div>
+
+                    </div>
+                  </li>
+                </ul>
+              ))
+            }
           </div>
         </div>
       </div>
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,49 +1,64 @@
-import { LiaCartPlusSolid } from "react-icons/lia"
-import style from '../typography.module.css'
-// import { FiMinusCircle, FiPlusCircle } from "react-icons/fi";
-// import { useState } from "react";
+import { LiaCartPlusSolid } from "react-icons/lia";
+import style from "../typography.module.css";
+import { FiMinusCircle, FiPlusCircle } from "react-icons/fi";
 
-interface ButtonProps{
-    item: ItemProps;
-    onClick: (item: ItemProps) => void
+interface ButtonProps {
+  item: ItemProps;
+  onClick: (item: ItemProps) => void;
+  onIncrease: () => void;
+  onDecrease: () => void;
+  amount: number;
+  isAdded: boolean;
 }
 
 export interface ItemProps {
-    image: {
-        thumbnail: string;
-        mobile: string;
-        tablet: string;
-        desktop: string;
-    };
-    name: string;
-    category: string;
-    price: number;
-    amount: number;
-    total: number;
+  image: {
+    thumbnail: string;
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  name: string;
+  category: string;
+  price: number;
+  amount: number;
+  total: number;
 }
 
-export function Button( {item, onClick}: ButtonProps ) {
-    // const [activeButton, setActiveButton] = useState(false)
-    
+export function Button({ item, onClick, isAdded, onDecrease, onIncrease, amount }: ButtonProps) {
+  // const [activeButton, setActiveButton] = useState(false)
 
-    return(
-        <>
-            <button 
-                className=" flex items-center gap-2 py-3 px-7 border border-rose-400 rounded-[999px] absolute bottom-[-23px] bg-white cursor-pointer "
-                onClick={() => {onClick(item)}}
-                >
-                <LiaCartPlusSolid className=" text-red text-[20px] " />
-                <span
-                className={`${style["text-present-4-bold"]} text-rose-900 `}
-                >
-                Add to cart
-                </span>
-            </button>
-            {/* <div className=" flex items-center justify-between bg-red py-3 px-7 w-full max-w-[163px] rounded-[999px] text-white absolute bottom-[-23px] " >
-                <button className=" hover:bg-white transition-all duration-500 hover:text-red rounded-full cursor-pointer " ><FiMinusCircle /></button>
-                <span className={`${style['text-present-4-bold']}`} >0</span>
-                <button className="hover:bg-white transition-all duration-500 hover:text-red rounded-full cursor-pointer " ><FiPlusCircle /></button>
-            </div> */}
-        </>
-    )
+  
+
+  return (
+    <>
+      {isAdded ? (
+        <div className=" flex items-center justify-between bg-red py-3 px-7 w-full max-w-[163px] rounded-[999px] text-white absolute bottom-[-23px] ">
+          <button 
+            onClick={() => onDecrease()}
+            className=" hover:bg-white transition-all duration-500 hover:text-red rounded-full cursor-pointer ">
+            <FiMinusCircle />
+          </button>
+          <span className={`${style["text-present-4-bold"]}`}> {amount} </span>
+          <button 
+            onClick={() => onIncrease()}
+            className="hover:bg-white transition-all duration-500 hover:text-red rounded-full cursor-pointer ">
+            <FiPlusCircle />
+          </button>
+        </div>
+      ) : (
+        <button
+          className=" flex items-center gap-2 py-3 px-7 border border-rose-400 rounded-[999px] absolute bottom-[-23px] bg-white cursor-pointer "
+          onClick={() => {
+            onClick(item);
+          }}
+        >
+          <LiaCartPlusSolid className=" text-red text-[20px] " />
+          <span className={`${style["text-present-4-bold"]} text-rose-900 `}>
+            Add to cart
+          </span>
+        </button>
+      )}
+    </>
+  );
 }
